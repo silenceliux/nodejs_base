01f23665a9cd141bc61ff33394dbdbd11715bbed
silenceliux <370088307@qq.com>
2017-03-16 16:59:53 +0800

    ignore

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require("http");
+var fs  = require("fs");
+var events = require('events');
 http.createServer(function(req, res){
     res.writeHead(200,{'Content-Type':"text/plain"});
     res.end("hello world");
 }).listen(8080);
-console.log("runing");
+
+var data = fs.readFileSync("input.txt");//同步
+console.log(data);//输出buffer值
+console.log(data.toString());
+fs.readFile('input.txt',function(err, data){//异步
+    if(err) return console.error(err);
+    console.log(data.toString());
+});
